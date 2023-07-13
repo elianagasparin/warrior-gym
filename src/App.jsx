f84fd7c0576b9b13b498d3f997d7773dc290e98e
elianagasparin <elianagasparinquero@gmail.com>
2023-07-13 09:21:00 +0200

    base

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from '../public/icono.svg'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={logo} className="logo" alt="Warrior Gym Logo" />
+      </div>
+    </>
+  )
+}
+
+export default App
