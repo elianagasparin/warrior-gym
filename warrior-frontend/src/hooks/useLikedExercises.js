56402ec4dc0f95dde622eb0c0c67eac48b75d22e
Lkyha <lykoshunter@gmail.com>
2023-08-13 21:48:14 +0200

    LikesStyle actualizado

--- warrior-frontend/src/hooks/useLikedExercises.js
+++ warrior-frontend/src/hooks/useLikedExercises.js
@@ -1,21 +1,16 @@
 import { useEffect, useState } from "react";
-import { AuthContext } from "../context/AuthContext";
 
 const useLikedExercises = () => {
     const [likedExercises, setLikedExercises] = useState([]);
-    const { token } = useContext(AuthContext);
 
     useEffect(() => {
         const fetchLikedExercises = async () => {
             try {
-                const response = await fetch(
-                    `${import.meta.env.VITE_API_HOST}/likes`,
-                    {
-                        headers: {
-                            Authorization: localStorage.getItem("token"),
-                        },
-                    }
-                );
+                const response = await fetch(`http://localhost:3000/likes`, {
+                    headers: {
+                        Authorization: localStorage.getItem("token"),
+                    },
+                });
 
                 if (!response.ok) {
                     throw new Error(
