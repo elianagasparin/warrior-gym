afb0771a08f4890eefb93a0a50d151651c923ad6
elianagasparin <elianagasparinquero@gmail.com>
2023-08-14 00:19:39 +0200

    SearchPage actualizada

--- warrior-frontend/src/pages/AllExercisesPage.jsx
+++ warrior-frontend/src/pages/AllExercisesPage.jsx
@@ -1,11 +1,13 @@
 // Página en la que vemos todos los ejercicios disponibles
 
+import './AllExercisesPage.css'
 import { NavBar } from "../components/NavBar/NavBar";
 import { useExercises } from "../hooks/useExercises";
 import { NewExercise } from '../components/Exercises/NewExercise';
 import { ExerciseList } from '../components/Exercises/ExerciseList';
 import { Footer } from "../components/Footer"
-import { PageTitle } from "../components/PageTitle";
+import { LikeButton } from '../components/Actions/Likes/LikeButton';
+import { NewExercise } from '../components/Actions/Exercises/NewExercise';
 
 export const AllExercisesPage = () => {
   const { exercises, loading, error, addExercises } = useExercises();
