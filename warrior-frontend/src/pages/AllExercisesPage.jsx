016d237c937b1e0e666c07c38b13ffb0659efe09
elianagasparin <elianagasparinquero@gmail.com>
2023-08-14 00:19:39 +0200

    SearchPage actualizada

--- warrior-frontend/src/pages/AllExercisesPage.jsx
+++ warrior-frontend/src/pages/AllExercisesPage.jsx
@@ -1,5 +1,6 @@
 // Página en la que vemos todos los ejercicios disponibles
 
+import './AllExercisesPage.css'
 import { NavBar } from "../components/NavBar/NavBar";
 import { useExercises } from "../hooks/useExercises";
 import { NewExercise } from '../components/Exercises/NewExercise';
